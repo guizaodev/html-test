4470c4ee3752dddbfd87ebebaf2a5890e2e08bbc
Guilherme Marques <guilherme@faladev.org>
2023-08-15 01:37:10 -0300

    mudança de layout

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Seletor para o botão
+    const button = document.querySelector('.button');
+
+    // Evento de clique no botão
+    button.addEventListener('click', function() {
+        alert('Você clicou no botão!');
+    });
+});
